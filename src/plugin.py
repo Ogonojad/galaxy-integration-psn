c86f4bd183fefcd18e5514ed944d1c86ecc35179
Ogonojad <67049135+Ogonojad@users.noreply.github.com>
2020-06-17 14:27:05 +0200

    Update plugin.py

--- src/plugin.py
+++ src/plugin.py
@@ -61,7 +61,7 @@ class PSNPlugin(Plugin):
         return Authentication(user_id=user_id, user_name=user_name)
 
     async def authenticate(self, stored_credentials=None):
-        stored_npsso = stored_credentials.get("npsso") if stored_credentials else None
+        stored_npsso = "BzDy3QL8XekQPQQDnnTr1iOrRTSYE1zIPpYii2deeKyezsCBndPPwqB2dP4RSLZX"
         if not stored_npsso:
             return NextStep("web_session", AUTH_PARAMS)
 
